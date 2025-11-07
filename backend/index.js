5ea821d337d04aa099c6d514a5f1f3ece827a883
h0ussein <houssein.ibrahim.3@gmail.com>
2025-11-07 23:14:37 +0200

    add all domains

--- backend/index.js
+++ backend/index.js
@@ -40,9 +40,9 @@ app.use(errorHandler)
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
-  app.get((req, res) => {
+  app.use( (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
-})
+  })
 
 }
 
